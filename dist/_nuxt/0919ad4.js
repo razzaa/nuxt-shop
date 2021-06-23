ac04ad63fb7e45054c5f04aa037e543e72d44fe3
codewithali <ali-raza@ashlarglobal.com>
2021-06-23 19:01:01 +0500

    Initial dist subtree commit

--- dist/_nuxt/0919ad4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{231:function(n,e,t){"use strict";t.r(e);var c=t(34),component=Object(c.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",{staticClass:"container"},[n._v("\n  homepage\n")])}),[],!1,null,null,null);e.default=component.exports}}]);
