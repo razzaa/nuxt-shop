bc8d909d40e617b1094ea695e6da2451648a63b5
codewithali <ali-raza@ashlarglobal.com>
2021-06-24 11:50:41 +0500

    Initial dist subtree commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,12 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+    script: [
+      {
+        src: 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js'
+      }
     ]
   },
 
