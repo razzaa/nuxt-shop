0cdc28a4d24a022d8032a00a13d32be24bf7cc5f
codewithali <ali-raza@ashlarglobal.com>
2021-06-24 16:39:03 +0500

    Initial dist subtree commit

---
+++ _nuxt/static/1624534726/login/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-shop\u002F_nuxt\u002Fstatic\u002F1624534726",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Flogin",config:{_app:{basePath:"\u002Fnuxt-shop\u002F",assetsPath:"\u002Fnuxt-shop\u002F_nuxt\u002F",cdnURL:a}}}}(null));
