837136b510241298b2b7c5701e7f74081f8f0030
codewithali <ali-raza@ashlarglobal.com>
2021-08-11 10:56:05 +0500

    base url set root

--- _nuxt/static/1628661078/signup/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628661078",layout:"default",error:a,state:{auth:{user:a,loggedIn:false,strategy:"local"}},serverRendered:true,routePath:"\u002Fsignup",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
